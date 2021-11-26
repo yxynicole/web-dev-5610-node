9bb3244a77bd538d9456b77477b16c6205fa887d
yxynicole <ye.xinyu@northeastern.edu>
2021-11-25 22:04:54 -0500

    added deleteMovie

--- movies/dao.js
+++ movies/dao.js
@@ -1,5 +1,14 @@
-const model = require('./model');               // load model
-const findAllMovies = () => model.find();      // use model's find function to implement findAllMovies
+// load model
+const model = require('./model');
+// use model's find function to implement findAllMovies
+const findAllMovies = () => model.find();
+
+// implement function to remove a movie given its ID
+const deleteMovie = (id) =>{
+    // use mongoose model removeOne function to remove movie by ID
+    return model.deleteOne({_id: id});
+}
+
 module.exports ={
-    findAllMovies                              // export as an API to use in service
+    findAllMovies,deleteMovie
 }
--- movies/service.js
+++ movies/service.js
@@ -1,8 +1,24 @@
-const dao = require('./dao')                                //load dao
-module.exports = (app) => {                                //accept express instance to create HTTP endpoints
-    const findAllMovies = (req, res) => {                   //handle HTTP GET request
-        dao.findAllMovies()                                 //use dao to retrieve all movies
-            .then(movies=>res.json(movies));       //respond with all movies
+const dao = require('./dao')
+
+//accept express instance to create HTTP endpoints
+module.exports = (app) => {
+    //handle HTTP GET request
+    const findAllMovies = (req, res) => {
+        //use dao to retrieve all movies
+        dao.findAllMovies()
+            //respond with all movies
+            .then(movies=>res.json(movies));
     }
-    app.get("/rest/movies",findAllMovies)                   //listen for HTTP GET and notify findAllMovies
+
+    // handle HTTP DELETE request mapped to /rest/movies/:id
+    const deleteMovie = (req, res) =>{
+        //console.log("server, service.js: ------>", req)
+        // remove movie from database with ID matching ID in params
+        dao.deleteMovie(req.params.id).then((status) => res.send(status));
+    }
+
+
+    app.delete("/rest/movies/:id", deleteMovie);
+    //listen for HTTP GET and notify findAllMovies
+    app.get("/rest/movies",findAllMovies)
 }
